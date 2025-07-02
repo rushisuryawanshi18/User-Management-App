d85015844e77a26ebeb04825716c1af1cab08799
rushisuryawanshi <rushisuryawanshi2593@gmail.com>
2025-07-02 13:55:43 +0530

    DB Connection

--- src/main/java/com/userapp/util/DBConnection.java
+++ src/main/java/com/userapp/util/DBConnection.java
@@ -1,5 +1,24 @@
 package com.userapp.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBConnection {
+
+	public static Connection getConnection() {
+		Connection conn = null;
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/userdb", "root", "root");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return conn;
+
+	}
 
 }
